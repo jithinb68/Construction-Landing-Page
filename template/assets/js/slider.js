7d9aca6107db76fd498a197d34ce1a1047aa5526
jithin68 <jithin68@Jithin.local>
2020-05-25 18:45:16 +0530

    ;eft sidebar

---
+++ template/assets/js/slider.js
@@ -0,0 +1,29 @@
+$('.rangeslider3').ionRangeSlider({
+    type: 'double',
+   
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 600,
+    prefix: '$'
+});
+
+$('#example-css').barrating({
+    theme: 'css-stars',
+    showSelectedRating: false
+  });
+
+  $('#example-css1').barrating({
+    theme: 'css-stars',
+    showSelectedRating: false
+  });
+
+  $('#example-css2').barrating({
+    theme: 'css-stars',
+    showSelectedRating: false
+  });
+
+  $('#example-css3').barrating({
+    theme: 'css-stars',
+    showSelectedRating: false
+  });
--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,18 +1,34 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sourceMaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync')
 const del = require('del');
 
-gulp.task('styles', () => {
+function styleMain() {
     return gulp.src('assets/scss/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('assets/css/'));
-});
+    .pipe(sourceMaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourceMaps.write('/'))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('clean', () => {
-    return del([
-        'css/main.css',
-    ]);
-});
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './',
+        },
+        startPath: 'pages/filter.html',
+        ghostMode: false,
+        notify: false
+    });
+    gulp.watch('./assets/scss/components/*.scss', styleMain);
+    gulp.watch('./pages/*.html').on('change', browserSync.reload);
+    gulp.watch('./src/js/**/*.js').on('change', browserSync.reload);
+
+}
 
 function cleanVendors(){
     return del('assets/vendors/**/*');
@@ -23,10 +39,25 @@ function buildVendors() {
                      .pipe(gulp.dest('assets/vendors/bootstrap'));
     var addon2 = gulp.src('./node_modules/@mdi/**/*')
                      .pipe(gulp.dest('assets/vendors/@mdi'));
+    var addon3 = gulp.src(['./node_modules/ion-rangeslider/js/ion.rangeSlider.min.js'])
+                     .pipe(gulp.dest('assets/vendors/ion-rangeslider/js'));
+    var addon4 = gulp.src(['./node_modules/ion-rangeslider/css/ion.rangeSlider.css'])
+                     .pipe(gulp.dest('assets/vendors/ion-rangeslider/css'));
+    var addon5 = gulp.src(['./node_modules/ion-rangeslider/img/*'])
+                     .pipe(gulp.dest('assets/vendors/ion-rangeslider/img'));
+    var addon6 = gulp.src(['./node_modules/jquery/dist/jquery.min.js'])
+                     .pipe(gulp.dest('assets/vendors/jquery'));
+    var addon7 = gulp.src(['./node_modules/font-awesome/fonts/*'])
+                     .pipe(gulp.dest('./assets/vendors/font-awesome/fonts'));
+    var addon8 = gulp.src(['./node_modules/jquery-bar-rating/dist/jquery.barrating.min.js'])
+                    .pipe(gulp.dest('./assets/vendors/jquery-bar-rating'));
+    var addon9 = gulp.src(['./node_modules/jquery-bar-rating/dist/themes/css-stars.css'])
+                    .pipe(gulp.dest('./assets/vendors/jquery-bar-rating'));
 
-    return (addon1, addon2);
+    return (addon1, addon2, addon3, addon4, addon5, addon6, addon7, addon8, addon9);
 }
 
-gulp.task('default', gulp.series(['clean', 'styles']));
-
+// gulp.task('default', gulp.series(['clean', 'styles']));
+exports.style = styleMain;
+exports.watch = watch;
 exports.buildVendors = gulp.series(cleanVendors, buildVendors);
